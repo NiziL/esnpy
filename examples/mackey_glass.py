555a05ee22d2ccf4a5c1c992bd6900ad08f79496
NiZiL <biasutto.t@gmail.com>
2016-11-02 18:06:04 +0100

    mackeyglass: cleaning + plot

--- examples/mackey_glass.py
+++ examples/mackey_glass.py
@@ -3,42 +3,73 @@
 import esnpy
 from esnpy import batch_trainer
 import numpy as np
+import pylab
 
 
 def main():
+    ## Parameters
+    # reservoir
+    size = 1000
+    leaky = 0.1
+    func = np.tanh
+    input_init = esnpy.init.UniformDenseInit(-0.5, 0.5)
+    intern_init = esnpy.init.UniformSparseInit(-0.5, 0.5, 0.01)
+    spectral_radius = esnpy.tuner.SpectralRadiusSetter(1.25)
+
+    # learning
+    transient_len = 100
+    learn_len = 2000
+    ridge_regul_coef = 1e-8
+
+    # testing
+    test_len = 500
+
     print("Loading data...")
     data = np.loadtxt('data/MackeyGlass_t17.txt')
 
     print("Creating a reservoir...")
     builder = esnpy.ReservoirBuilder()
-    builder.set_leaky(0.3)\
-           .set_activation_function(np.tanh)\
+    builder.set_leaky(leaky)\
+           .set_activation_function(func)\
            .set_input_size(1)\
-           .set_input_init(esnpy.init.UniformDenseInit(-0.5, 0.5))\
-           .set_intern_size(1000)\
-           .set_intern_init(esnpy.init.UniformSparseInit(-0.5, 0.5, 0.01))\
-           .add_tuner(esnpy.tuner.SpectralRadiusSetter(1.25))\
+           .set_input_init(input_init)\
+           .set_intern_size(size)\
+           .set_intern_init(intern_init)\
+           .add_tuner(spectral_radius)
 
     reservoir = builder.build()
     esn = esnpy.ESN(reservoir)
 
     print("Training...")
+    transient_data = data[:transient_len, None]
+    learn_data = data[transient_len:learn_len, None]
+    target_data = data[transient_len+1:learn_len+1, None]
+
     training = esnpy.train.BatchTraining()
-    training.train(esn, batch_trainer.Ridge(1e-8),
-                   transient_data = data[:100, None],
-                   learning_data = data[100:2000, None],
-                   target = data[101:2001, None])
+    training.train(esn,
+                   batch_trainer.Ridge(1e-8),
+                   transient_data,
+                   learn_data,
+                   target_data)
 
     print("Testing...")
-    err = np.zeros((1, 500))
-    u = data[2000, None]
-    for i in range(500):
-        y = esn.predict(u)
-        err = data[2000+i, None]-y
-        u = y
+    test_data = data[learn_len:learn_len+test_len, None]
+    test_target = data[learn_len+1:learn_len+1+test_len, None]
+    prediction = np.zeros((test_len, 1))
 
-    print('MSE: '+str(np.sum(np.square(err))/500))
-        
+    u = test_data[0, :]
+    for i in range(test_len):
+        prediction[i, :] = esn.predict(u)
+        u = prediction[i, :]
+
+    mse = np.mean(np.square(test_target-prediction))
+    print('MSE: '+str(mse))
+
+    pylab.plot(test_target, label='target')
+    pylab.plot(prediction, label='prediction')
+    pylab.legend()
+    pylab.show()
+
 
 if __name__ == '__main__':
     main()
