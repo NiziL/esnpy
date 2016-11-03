1c4f8e54c9b0cd0f062edeeb95e0f1cfb047f6d5
NiZiL <biasutto.t@gmail.com>
2016-11-03 20:05:35 +0100

    cleaning the trajectories example

--- examples/trajectories.py
+++ examples/trajectories.py
@@ -19,12 +19,12 @@ def class3(t, beta):
 
 
 def prepare_data(nb_by_class, traj_size):
-    clazz = [(class1, [1,0,0]),
-             (class2, [0,1,0]), 
-             (class3, [0,0,1])]
+    clazz = [(class1, (0,)),
+             (class2, (1,)), 
+             (class3, (2,))]
 
     data = np.zeros((nb_by_class*traj_size, 2))
-    target = np.zeros((nb_by_class*traj_size, 3))
+    target = np.zeros((nb_by_class*traj_size, 1))
     for i in range(nb_by_class):
         np.random.shuffle(clazz)
         d = clazz[0][0](np.linspace(0, 2*np.pi, traj_size), np.random.rand()*2*np.pi)
@@ -38,49 +38,61 @@ def main():
     traj_size = 30
     nb_by_class = 50
 
+    print("Loading data...")
     learn_data, learn_target = prepare_data(nb_by_class, traj_size)
 
+    print("Creating a reservoir...")
     builder = esnpy.ReservoirBuilder()
     builder.set_leaky(0.8)\
            .set_input_size(2)\
-           .set_input_init(esnpy.init.UniformDenseInit(-0.5, 0.5))\
+           .set_input_init(esnpy.init.UniformDenseInit(-2, 2))\
            .set_intern_size(1000)\
-           .set_intern_init(esnpy.init.UniformSparseInit(-0.5, 0.5, 0.01))\
-           .add_tuner(esnpy.tuner.SpectralRadiusSetter(1.25))
+           .set_intern_init(esnpy.init.NormalSparseInit(0, 1, 0.01))\
+           .add_tuner(esnpy.tuner.SpectralRadiusSetter(1.3))
 
     reservoir = builder.build()
 
+    models = [linear_model.LogisticRegression(),
+              linear_model.SGDClassifier(loss='log'),
+              linear_model.SGDClassifier(loss='hinge'),
+              linear_model.SGDClassifier(loss='modified_huber'),
+              linear_model.SGDClassifier(loss='perceptron')]
+    trainers = list(map(lambda x: batch_trainer.SklearnReg(x), models))
+    trainer_names = ["Logistic regression",
+                     "SGD log",
+                     "SGD hinge",
+                     "SGD huber",
+                     "SGD perceptron"]
+
+    print("Starting the batch training...")
     training = esnpy.train.BatchTraining()
-    trainers = [ batch_trainer.Ridge(1e-5),
-                 batch_trainer.SklearnReg(linear_model.LogisticRegression())]
-    trainer_names = ["ridge", "sklog"]
-
     batch = training.batch(reservoir)\
-                   .transient(learn_data[:traj_size :])\
-                   .memorize(learn_data[traj_size:, :])
-    for trainer in trainers[:-1]:
-        batch.train_readout(trainer, learn_target[traj_size:, :])
-    batch.train_readout(trainers[-1], np.argmax(learn_target[traj_size:, :], axis=1))
+                    .transient(learn_data[:traj_size :])\
+                    .memorize(learn_data[traj_size:, :])
+    for name, trainer in zip(trainer_names, trainers):
+        print("Training \"{}\"...".format(name))
+        batch.train_readout(trainer, learn_target[traj_size:, 0])
 
-    readout = batch.get_readout()
+    readout_layers = batch.get_readout()
 
+    print("Testing...")
+    nb_by_class = 150
     data, target = prepare_data(nb_by_class, traj_size)
-    predictions = np.zeros((nb_by_class*traj_size, len(readout)))
+    predictions = np.zeros((nb_by_class*traj_size, len(readout_layers)))
     for i in range(traj_size*nb_by_class):
         reservoir.update(data[i, :])
-        for j in range(len(readout)-1):
-            predictions[i, j] = np.argmax(readout[j].compute(reservoir))
-        predictions[i, -1] = readout[-1].compute(reservoir)
+        for j, readout in enumerate(readout_layers):
+            predictions[i, j] = readout.compute(reservoir)
 
-    errors = np.zeros((nb_by_class*traj_size, len(readout)))
-    for i in range(traj_size*nb_by_class):
-        errors[i, :] = predictions[i, :] == np.argmax(target[i, :])
+    errors = np.zeros((nb_by_class*traj_size, len(readout_layers)))
+    for j, prediction in enumerate(predictions.T):
+        errors[:, j] = prediction == target[:, 0]
 
-    step_acc = np.zeros((traj_size, len(readout)))
+    step_acc = np.zeros((traj_size, len(readout_layers)))
     for k in range(traj_size):
         step_acc[k, :] = np.mean(errors[k::traj_size, :], axis=0)
 
-    for i, acc in zip(range(len(readout)), step_acc.T):
+    for i, acc in enumerate(step_acc.T):
         print("{} readout's accuracy: {}\n{}".format(trainer_names[i], np.mean(acc), acc))
 
 
